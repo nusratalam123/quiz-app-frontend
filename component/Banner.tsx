7a2d79e9e5adcbf7815853f29cf7889ac54a85e3
nusratalam <nusratalam975@gmail.com>
2024-07-07 02:16:07 +0600

    add Baner and Update navbar

---
+++ component/Banner.tsx
@@ -0,0 +1,13 @@
+// components/Banner.tsx
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1 style={{ margin: "20px" }}>Welcome to Quiz App</h1>
+      <p style={{ margin: "20px" }}>
+        Challenge yourself with quizzes on various topics!
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -1,12 +1,35 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/login">Login</Link>
-      <Link href="/register">Register</Link>
-      <Link href="/admin">Admin</Link>
+    // <nav>
+    //   <Link href="/">Home</Link>
+    //   <Link href="/login">Login</Link>
+    //   <Link href="/register">Register</Link>
+    //   <Link href="/admin">Admin</Link>
+    // </nav>
+
+    <nav className="navbar">
+      <div style={{ margin: "20px" }}>
+        <span>Quizo App</span>
+      </div>
+      {/* <div className="nav-links">
+        <Link href="/">Home</Link>
+        <Link href="/quizzes">Quizzes</Link>
+        <Link href="/profile">Profile</Link>
+      </div> */}
+      <div className="auth-buttons">
+        <Link href="/">
+          <button>Home</button>
+        </Link>
+        <Link href="/login">
+          <button>Login</button>
+        </Link>
+        <Link href="/register">
+          <button>Register</button>
+        </Link>
+      </div>
     </nav>
   );
 };

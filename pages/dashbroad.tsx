c752aa468ab11d219a9207ab42c3e06682086e6f
nusratalam <nusratalam975@gmail.com>
2024-07-07 13:43:47 +0600

    Update dashbroad pannel

---
+++ pages/dashbroad.tsx
@@ -0,0 +1,110 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+
+const Dashboard = () => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    router.push("/login");
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div style={{ margin: "20px" }}>
+          <span>Quizo App</span>
+        </div>
+
+        <div className="auth-buttons">
+          <Link href="/">
+            <button>Home</button>
+          </Link>
+          <Link href="/login">
+            <button>Login</button>
+          </Link>
+
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      </nav>
+      <h2 style={{ textAlign: "center", marginBottom: "20px" }}>
+        <h1>Dashboard</h1>
+      </h2>
+      <div >
+        <Link href="/quiz">
+          <button
+            className="login-button"
+            style={{
+              width: "30%",
+              marginLeft:"35%",
+              padding: "10px",
+              backgroundColor: "#0070f3",
+              color: "#fff",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+              fontSize: "1rem",
+            }}
+          >
+            Take a Quiz
+          </button>
+        </Link>
+        <Link href="/admin">
+          <button
+            className="login-button"
+            style={{
+              width: "30%",
+              padding: "10px",
+              backgroundColor: "#0070f3",
+              marginLeft: "35%",
+              marginTop: "2%",
+              color: "#fff",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+              fontSize: "1rem",
+            }}
+          >
+            Admin Panel
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
+
+
+// import { useRouter } from "next/router";
+// import Link from "next/link";
+// import Navbar from "../component/Navbar";
+
+// const Dashboard = () => {
+//   const router = useRouter();
+
+//   const handleLogout = () => {
+//     localStorage.removeItem("token");
+//     router.push("/login");
+//   };
+
+//   return (
+//     <div>
+//       <Navbar />
+//       <h1>Dashboard</h1>
+//       <button onClick={handleLogout}>Logout</button>
+//       <div>
+//         <Link href="/quiz">
+//           Take a Quiz
+//         </Link>
+//         <Link href="/admin">
+//           Admin Panel
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
+
